173289fc8dfa89c5858bd7bf95982f25df1a7508
Alma Kristiāna Vītola <69206400+AKVitola@users.noreply.github.com>
2020-10-18 22:31:49 +0300

    Progress with video

---
+++ assets/js/theme.js
@@ -0,0 +1,4 @@
+document.getElementById("js-video").addEventListener("click", function() {
+  this.innerHTML = '<iframe src="https://sparkleheart.org/wp-content/uploads/2020/09/SparkleHeart_animacija_V4.mp4" type="video/mp4" width="1366px" height="722px" frameborder="0">';
+  this.className = "active";
+});
